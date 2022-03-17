0bf8ff4dd3664a391395e840722da4fda400cce7
elifb <elifb@DESKTOP-R9ITCEQ>
2022-03-17 19:48:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> engTurk = new Dictionary<string, string>();
+            engTurk.Add("Work", "Çalış");
+            engTurk.Add("Patience", "Sabır");
+            foreach (var words in engTurk)
+            {
+                Console.WriteLine(words);
+
+            }
+
+            foreach (var words in engTurk)
+            {
+                Console.WriteLine(words);
+                Console.WriteLine(words.Value);//only value
+                Console.WriteLine(words.Key); // only key
+
+
+            }
+            Console.WriteLine(engTurk.ContainsKey("book")); //Does the dictionary contain "book"?
+            Console.ReadLine();
+
+        }
+    }
+}
